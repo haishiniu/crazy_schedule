fb81a392db80da75b4bc52d193f3123201e19d2b
haishiniu <609463803@qq.com>
2020-06-01 16:07:36 +0800

    add new dec

---
+++ src/part_I/__init__.py
@@ -0,0 +1,8 @@
+# -*- encoding: utf-8 -*-
+# !/usr/bin/python
+"""
+@File    : __init__.py.py
+@Time    : 2020/6/1 15:37
+@Author  : haishiniu
+@Software: PyCharm
+"""
---
+++ src/part_I/single_thread.py
@@ -0,0 +1,32 @@
+# -*- encoding: utf-8 -*-
+# !/usr/bin/python
+"""
+@File    : single_thread.py
+@Time    : 2020/6/1 15:37
+@Author  : haishiniu
+@Software: PyCharm
+"""
+from threading import Thread
+import time
+
+
+def my_counter():
+    i = 0
+    for _ in range(100000000):
+        i = i + 1
+    return True
+
+
+def main():
+    thread_array = {}
+    start_time = time.time()
+    for tid in range(2):
+        t = Thread(target=my_counter)
+        t.start()
+        t.join()
+    end_time = time.time()
+    print("Total time: {}".format(end_time - start_time))
+
+
+if __name__ == '__main__':
+    main()
